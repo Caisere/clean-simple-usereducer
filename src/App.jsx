23bbfad54c782efb4afe8d71ebf256bffd7a25c2
Caisere <heylehgbedey@gmail.com>
2025-06-17 06:14:42 -0700

    fix: refractor states and actions

--- src/App.jsx
+++ src/App.jsx
@@ -3,60 +3,68 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 
 
+const initialValues = {
+  count: 0,
+  value: "",
+  showCount: false
+}
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'set':
+      {
+        console.log(state, action);
+        return { ...state, count: action.payload, showCount: true, value: "" };
+      }
+      break;
+    case 'count':
+      {
+        console.log(state, action);
+        return { ...state, value: action.payload };
+      }
+      break;
+    case 'reset': 
+      {
+        return {state}
+      }
+      break;
+    default: {
+      throw new Error('unknown action type')
+    }
+  }
+}
 
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // const [value, setValue] = useState(0)
-
-  const [state, dispatch] = useReducer(reducer, { count: 0, value: 0 });
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case 'set':
-        {
-          console.log(state, action);
-          return { ...state, count: action.payload };
-        }
-        break;
-      case 'count':
-        {
-          console.log(state, action);
-          return { ...state, value: action.payload };
-        }
-        break;
-      default: {
-        return state;
-      }
-    }
-  }
-
+  const [state, dispatch] = useReducer(reducer, initialValues);
+  const {count, value, showCount} = state;
   return (
     <div className="App">
-      <div>{state.count}</div>
+      {showCount && <div>{count}</div>}
       <input
-        type="number"
-        value={state.value}
+        type="text"
+        value={value}
         onChange={(e) => {
           console.log(e.target.value);
-          dispatch({ type: 'count', payload: e.target.value });
+          dispatch({ type: 'count', payload: Number(e.target.value) });
         }}
       />
       <div className="card">
         <button
           onClick={() => {
-            dispatch({ type: 'set', payload: state.value });
+            dispatch({ type: 'set', payload: value });
           }}
         >
           set
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={(e) =>{
+          dispatch({type: 'reset'})}
+        }>
+          reset
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   );
 }
